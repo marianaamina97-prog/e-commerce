0f99abdf38259720f40c869c2168f308b7a46b8b
Mariana Amina <marianaamina97@gmail.com>
2025-10-23 17:38:47 +0300

    h1 and p adjustment

--- e-commerce/src/components/sections/Hero.jsx
+++ e-commerce/src/components/sections/Hero.jsx
@@ -1,12 +1,18 @@
 function Hero() {
   return (
-    <section className="hero">
-        <div className="container mx-auto px-4 py-20 text-center">
-            <h1 className="text-4xl md:text-5xl font-bold mb-6">Illuminate Your World with Timeless Elegance</h1>
-            <p className="text-lg md:text-xl mb-8">Elevate Your home with premium lighting that combines
-                 high-end materials,contemporary design and lasting brilliance. </p>
-            <a href="/get-started" className="bg-blue-600 text-white px-6 py-3 rounded-full text-lg hover:bg-blue-700 transition duration-300">Shop Now</a>
-        </div>
+  <section className="hero">
+    {/* To vertically center the hero in the viewport */}
+    <div className="container mx-auto px-4 py-20 text-center">
+      <h1 className="text-4xl md:text-5xl font-bold mb-6 max-w-3xl mx-auto leading-tight">
+        <span className="block">Illuminate Your World With</span>
+        <span className="block">Timeless Elegance</span>
+      </h1>
+      <p className="text-lg md:text-xl mb-8 max-w-2xl mx-auto">
+        Elevate your home with premium lighting that combines high-end materials,
+        contemporary design, and lasting brilliance.
+      </p>
+      <a href="/get-started" className="bg-blue-600 text-white px-6 py-3 rounded-full text-lg hover:bg-blue-700 transition duration-300">Shop Now</a>
+    </div>
     </section>
   );
 };
